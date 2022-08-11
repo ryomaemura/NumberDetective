db7b81a554308939fffb2b4c8c1e6e2ecd01dad2
ryomaemura <ryomaemura34@gmail.com>
2022-08-11 14:51:21 +0900

    add input-number

--- Assets/Scenes/NumberManager.cs
+++ Assets/Scenes/NumberManager.cs
@@ -6,6 +6,24 @@ using TMPro;
 
 public class NumberManager : MonoBehaviour
 {
+    [SerializeField] Button button1;
+    [SerializeField] Button button2;
+    [SerializeField] Button button3;
+    [SerializeField] Button button4;
+    [SerializeField] Button button5;
+    [SerializeField] Button button6;
+    [SerializeField] Button button7;
+    [SerializeField] Button button8;
+    [SerializeField] Button button9;
+    [SerializeField] Button button10;
+    [SerializeField] Button button11;
+    [SerializeField] Button button12;
+    [SerializeField] Button button13;
+    [SerializeField] Button button14;
+    [SerializeField] Button button15;
+    [SerializeField] Button button16;
+    Button[] buttons;
+    int[] selectButtons = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     [SerializeField] TextMeshProUGUI buttonText1;
     [SerializeField] TextMeshProUGUI buttonText2;
     [SerializeField] TextMeshProUGUI buttonText3;
@@ -27,31 +45,188 @@ public class NumberManager : MonoBehaviour
     [SerializeField] Button minButton;
     [SerializeField] Button sumButton;
     Button[] modeButtons;
+    [SerializeField] TextMeshProUGUI moveText;
+    int moveNumber = 0;
+    [SerializeField] TextMeshProUGUI resultText;
     int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
     // mode : max, min, sum
     int[] modeBoolean = {0, 0, 0};
+    int modeFlag = 0;
+    int[] addNumbers = {0, 1, 4, 5};
+    int maxNumber = 0;
+    int minNumber = 100;
+    int sumNumber = 0;
+    [SerializeField] TMP_InputField numberInputField;
+    [SerializeField] Button submitButton;
+    [SerializeField] TextMeshProUGUI descriptionText;
+    int inputNumber = 0;
+    int setFlag = 0;
 
     // Start is called before the first frame update
     void Start() {
+        buttons = new Button[] {button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15, button16};
         buttonTexts = new TextMeshProUGUI[] {buttonText1, buttonText2, buttonText3, buttonText4, buttonText5, buttonText6, buttonText7, buttonText8, buttonText9, buttonText10, buttonText11, buttonText12, buttonText13, buttonText14, buttonText15, buttonText16};
         modeButtons = new Button[] {maxButton, minButton, sumButton};
+
+        resetNumberInputField();
+        resetSubmitButton();
     }
 
     // Update is called once per frame
     void Update() {
     }
 
+    public void resetNumberButton() {
+        for (int i = 0; i < buttons.Length; i++) {
+            buttons[i].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            selectButtons[i] = 0;
+        }
+    }
+
+    public void resetModeButton() {
+        for (int i = 0; i < modeBoolean.Length; i++) {
+            modeBoolean[i] = 0;
+            modeButtons[i].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+        }
+    }
+
+    public void resetNumberInputField() {
+        numberInputField.GetComponent<Image>().color = new Color32(100, 100, 100, 255);
+    }
+
+    public void resetSubmitButton() {
+        submitButton.GetComponent<Image>().color = new Color32(100, 100, 100, 255);
+    }
+
     public void clickModeButton(int modeNumber) {
         if (modeBoolean[modeNumber] == 0) {
-            for (int i = 0; i < modeBoolean.Length; i++) {
-                modeBoolean[i] = 0;
-                modeButtons[i].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
-            }
+            resetNumberButton();
+            resetModeButton();
+            resetNumberInputField();
+            resetSubmitButton();
+
             modeBoolean[modeNumber] = 1;
             modeButtons[modeNumber].GetComponent<Image>().color = new Color32(255, 100, 100, 255);
         } else {
-            modeBoolean[modeNumber] = 0;
-            modeButtons[modeNumber].GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            resetNumberButton();
+            resetModeButton();
+            resetNumberInputField();
+            resetSubmitButton();
         }
+    }
+
+    public void clickNumberButton(int buttonNumber) {
+        resetNumberInputField();
+        resetSubmitButton();
+
+        modeFlag = 0;
+        for (int i = 0; i < modeBoolean.Length; i++) {
+            if (modeBoolean[i] == 1) {
+                modeFlag = 1;
+                selectNumber(buttonNumber);
+            }
+        }
+
+        if (modeFlag == 0) {
+            resetNumberButton();
+
+            selectButtons[buttonNumber] = 1;
+            buttons[buttonNumber].GetComponent<Image>().color = new Color32(0, 200, 255, 255);
+            numberInputField.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+            submitButton.GetComponent<Image>().color = new Color32(255, 255, 255, 255);
+        }
+    }
+
+    public void selectNumber(int buttonNumber) {
+        if (selectButtons[buttonNumber] == 1) {
+            resetNumberButton();
+        } else if (
+            buttonNumber != 3 &
+            buttonNumber != 7 &
+            buttonNumber != 11 &
+            buttonNumber != 12 &
+            buttonNumber != 13 &
+            buttonNumber != 14 &
+            buttonNumber != 15
+        ) {
+            resetNumberButton();
+
+            for (int i = 0; i < addNumbers.Length; i++) {
+                buttons[buttonNumber + addNumbers[i]].GetComponent<Image>().color = new Color32(255, 100, 100, 255);
+                selectButtons[buttonNumber + addNumbers[i]] = 1;
+            }
+        }
+    }
+
+    public void setNumber() {
+        setFlag = 0;
+        int.TryParse(numberInputField.text, out inputNumber);
+
+        for (int i = 0; i < selectButtons.Length; i++) {
+            if (selectButtons[i] == 1 && numbers[i] == inputNumber) {
+                setFlag = 1;
+
+                buttonTexts[i].text = inputNumber.ToString();
+                descriptionText.text = "Correct answer";
+
+                resetNumberButton();
+                resetNumberInputField();
+                resetSubmitButton();
+            }
+        }
+
+        if (setFlag == 0) {
+            descriptionText.text = "Wrong number";
+        }
+    }
+
+    public void okButton() {
+        if (modeBoolean[0] == 1) {
+            // max mode
+            maxNumber = 0;
+
+            for (int i = 0; i < selectButtons.Length; i++) {
+                if (selectButtons[i] == 1 & maxNumber < numbers[i]) {
+                    maxNumber = numbers[i];
+                }
+            }
+
+            showResult(maxNumber);
+        } else if (modeBoolean[1] == 1) {
+            // min mode
+            minNumber = 100;
+
+            for (int i = 0; i < selectButtons.Length; i++) {
+                if (selectButtons[i] == 1 & minNumber > numbers[i]) {
+                    minNumber = numbers[i];
+                }
+            }
+
+            showResult(minNumber);
+        } else {
+            // sum mode
+            sumNumber = 0;
+
+            for (int i = 0; i < selectButtons.Length; i++) {
+                if (selectButtons[i] == 1) {
+                    sumNumber = sumNumber + numbers[i];
+                }
+            }
+
+            showResult(sumNumber);
+        }
+    }
+
+    public void showResult(int resultNumber) {
+        if (modeBoolean[0] == 1) {
+            resultText.text = "Max : " + resultNumber.ToString();
+        } else if (modeBoolean[1] == 1) {
+            resultText.text = "Min : " + resultNumber.ToString();
+        } else {
+            resultText.text = "Sum : " + resultNumber.ToString();
+        }
+
+        moveNumber = moveNumber + 1;
+        moveText.text = "Move : " + moveNumber.ToString();
     }
 }
